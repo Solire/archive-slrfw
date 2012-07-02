126248ebf55a23423eb6b5e5b5449c6d5f55522d
Stéphane Monnot <smonnot@debian.(none)>
2012-07-02 16:21:27 +0200

    Tableau de bord avec la liste des pages sous formes de datatables

---
+++ config/list/board.cfg.php
@@ -0,0 +1,39 @@
+<?
+
+$config = array(
+    "extra" => array(
+        "copy" => false,
+        "print" => false,
+        "pdf" => false,
+        "csv" => false,
+        "hide_columns" => false,
+    ),
+    "table" => array(
+        "name" => "traduction",
+        "title" => "Edition des traductions",
+        "title_item" => "traduction",
+        "suffix_genre" => "e",
+        "fixedheader" => true,
+    ),
+    "columns" => array(
+        array(
+            "name" => "cle",
+            "index" => true,
+            "show" => true,
+            "filter_field" => "text",
+            "title" => "Texte initial",
+        ),
+        array(
+            "name" => "valeur",
+            "editable" => true,
+            "show" => true,
+            "filter_field" => "text",
+            "title" => "Traduction",
+        ),
+        array(
+            "name" => "id_version",
+            "index" => true,
+            "filter" => BACK_ID_VERSION,
+        ),
+    ),
+);
