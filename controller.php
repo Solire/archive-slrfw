9af63664e9404c8cb846f773db5f8c11762e6f7e
Adrien <aimbert@solire.fr>
2013-08-21 09:37:26 +0200

    ajout de la possibilité de récupérer le rewriting hors controller

--- controller.php
+++ controller.php
@@ -397,6 +397,16 @@ class Controller
     }
 
     /**
+     * Renvois les informations de rewriting courante
+     *
+     * @return array
+     */
+    final public function getRewriting()
+    {
+        return $this->rew;
+    }
+
+    /**
      * Test si les valeurs du tableau sont dans les paramètres de la page
      *
      * @param array $inputs Liste des valeurs à contrôler
