3d28d896049b3448e49e149855a39de83e20dd5c
thansen <thansen@solire.fr>
2014-04-09 17:03:27 +0200

    Commentaire et nettoyage de la classe Format\Number

--- format/number.php
+++ format/number.php
@@ -25,7 +25,7 @@ class Number
      * Formate un prix pour un affichage
      *
      * @param float  $price    Prix à afficher
-     * @param bool   $show     Afficher ou non le ",00"
+     * @param bool   $show     Afficher ou non le ',00'
      * @param string $currency Nom de la monnaie utilisée
      *
      * @return string
@@ -41,51 +41,66 @@ class Number
     }
 
     /**
+     * Formate un float avec des zéros
      *
-     * @param float $number
-     * @param int $nbZero
-     * @param string $currencyChar Caractère de la devise
+     * @param float  $number       Float à formater
+     * @param int    $nbZero       Nombre de zero
+     *
      * @return string
      */
-    static function zeroFill($number, $nbZero = 11) {
-        return printf("%0" . $nbZero . "d", $number);
+    static function zeroFill($number, $nbZero = 11)
+    {
+        return printf('%0' . $nbZero . 'd', $number);
     }
 
     /**
+     * Formatage d'une taille en octet en Ko, Mo
      *
-     * @param type $valeur
+     * @param int $size Taille à reformater en octet
      *
-     * @return type
+     * @return string
      */
-    static function formatSize($valeur) {
-        $strTmp = "";
+    static function formatSize($size)
+    {
+        $strTmp = '';
 
-        if (preg_match("#^[0-9]{1,}$#", $valeur)) {
-            if ($valeur >= 1000000) {
-                // Taille supérieur à 1 MegaOctet
-                $strTmp = sprintf("%01.2f", $valeur / 1000000);
-                // Suppression des "0" en fin de chaine
-                $strTmp = preg_replace("#[\.]{1}[0]{1,}$#", "", $strTmp) . " Mo";
-            } else if ($valeur >= 1000) {
-                // Taille infÃ©rieur Ã  1 MegaOctet
-                $strTmp = sprintf("%01.2f", $valeur / 1000);
-                // Suppression des "0" en fin de chaine
-                $strTmp = preg_replace("#[\.]{1}[0]{1,}$#", "", $strTmp) . " Ko";
-            } else if ($valeur >= 0) {
-                // Taille infÃ©rieur Ã  1 KiloOctet
-                $strTmp = $valeur . " octect";
-                if ($valeur > 0)
-                    $strTmp .= "s";
-            }
-            else {
-                $strTmp = $valeur;
+        if (preg_match('`^[0-9]{1,}$`', $size)) {
+            if ($size >= 1000000) {
+                /**
+                 * Taille supérieur à 1 MegaOctet
+                 */
+                $strTmp = sprintf('%01.2f', $size / 1000000);
+
+                /**
+                 * Suppression des '0' en fin de chaine
+                 */
+                $strTmp = preg_replace('`[\.]{1}[0]{1,}$`', '', $strTmp) . ' Mo';
+            } else if ($size >= 1000) {
+                /**
+                 * Taille inférieur à 1 MegaOctet
+                 */
+                $strTmp = sprintf('%01.2f', $size / 1000);
+
+                /**
+                 * Suppression des '0' en fin de chaine
+                 */
+                $strTmp = preg_replace('`[\.]{1}[0]{1,}$`', '', $strTmp) . ' Ko';
+            } else if ($size >= 0) {
+                /**
+                 * Taille inférieur à  1 KiloOctet
+                 */
+                $strTmp = $size . ' octect';
+                if ($size > 0) {
+                    $strTmp .= 's';
+                }
+            } else {
+                $strTmp = $size;
             }
         } else {
-            $strTmp = $valeur;
+            $strTmp = $size;
         }
 
         return $strTmp;
     }
-
 }
 
