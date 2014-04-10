4e78e3f97ff4ce5dd5320b7eb11ec41416aa904e
Adrien <aimbert@solire.fr>
2014-04-10 12:12:44 +0200

    utilisation focntions php pour crypter les mdp

---
+++ .install/exec-9.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Ajout d'une clé sha256 dans al table traduction
+ *
+ * @package    Slrfw
+ * @subpackage Install
+ * @author     Shinbuntu <smonnot@solire.fr>
+ * @license    Solire http://www.solire.fr/
+ */
+
+set_include_path(
+    get_include_path()
+    . PATH_SEPARATOR . realpath(pathinfo(__FILE__, PATHINFO_DIRNAME) . '/../../')
+);
+
+require 'slrfw/init.php';
+
+\Slrfw\FrontController::init();
+
+$db = \Slrfw\Registry::get('db');
+
+
+// Ajout de la colonne cle_sha dans la table traduction
+$query = 'UPDATE utilisateur SET pass = "$2y$10$VfOIubFk/IjdqxPwL60xtOrxXwlOEQqSn5Ml0Jc5z0cSkenCgH8/e";';
+
+$db->exec($query);
+
