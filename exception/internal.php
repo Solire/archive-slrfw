febbfa942a4c14ceb0bfa670cfab9a78f4b6d3fe
Adrien <aimbert@solire.fr>
2014-06-25 17:45:09 +0200

    ajout d'une exception spéciale traitement interne

---
+++ exception/internal.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Exception spéciale pour traitement internes
+ *
+ * @author  Adrien <aimbert@solire.fr>
+ * @license CC by-nc http://creativecommons.org/licenses/by-nc/3.0/fr/
+ */
+
+namespace Slrfw\Exception;
+
+/**
+ * Exception spéciale pour traitement internes
+ *
+ * @author  Adrien <aimbert@solire.fr>
+ * @license CC by-nc http://creativecommons.org/licenses/by-nc/3.0/fr/
+ */
+class Internal extends \Exception
+{
+}
