c92745f77b752990d47ce358a5ba744130c0729b
Shinbuntu <smonnot@solire.fr>
2014-09-03 10:15:54 +0200

    Prise en compte de la  valeur par défaut d'un champ texte

--- model/gabarit/field/text/textfield.php
+++ model/gabarit/field/text/textfield.php
@@ -11,6 +11,15 @@ class TextField extends \Slrfw\Model\Gabarit\Field\GabaritField
 {
     public function start() {
         parent::start();
+
+        // Prise en compte de la valeur par défaut paramétrée
+        if(isset($this->params["VALUE.DEFAULT"]) 
+            && $this->params["VALUE.DEFAULT"]
+            && $this->idGabPage == 0
+        ) {
+            $this->value = $this->params["VALUE.DEFAULT"];
+        }
+
         if(isset($this->params["LINK"]) && $this->params["LINK"])
             $this->classes .= " autocomplete-link";
     }
