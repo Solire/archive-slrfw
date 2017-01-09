3e99922a8f77013e1c2eef466f6afa7035c92053
thansen <thansen@solire.fr>
2017-01-09 19:12:12 +0100

    Gestion erreur 40x

--- start.php
+++ start.php
@@ -21,12 +21,34 @@ try {
 } catch (Exception\User $exc) {
     Error::message($exc);
 } catch (Exception\HttpError $exc) {
-    if (current($exc->getHttp()) == '404') {
-        header('HTTP/1.0 404 Not Found');
+    $code = current($exc->getHttp());
+    if (in_array($code, [
+        401,
+        403,
+        404,
+    ])) {
+        switch ($code) {
+            case 401:
+                $header = 'HTTP/1.0 401 Unauthorized';
+                break;
+
+            case 403:
+                $header = 'HTTP/1.0 403 Forbidden';
+                break;
+
+            default :
+                $header = 'HTTP/1.0 404 Not Found';
+                break;
+        }
+
+        header($header);
         FrontController::run('Error', 'error404');
-    } else {
-        Error::http($exc->getHttp());
+
+        return;
     }
+
+    Error::http($exc->getHttp());
+
 } catch (\Exception $exc) {
     $marvin = new Marvin('debug', $exc);
     if ($debug) {
